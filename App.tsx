42b3f30aa25df4ad7836d5a008396d0f20650b13
natf1 <86424005+natf1@users.noreply.github.com>
2021-07-11 16:49:22 -0300

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -1,15 +1,82 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import {Agenda} from 'react-native-calendars';
+import {Card, Avatar} from 'react-native-paper';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+const timeToString = (time) => {
+  const date = new Date(time);
+  return date.toISOString().split('T')[0];
 }
+
+const App: React. FC = () => {
+  const [items,setItems] = useState({});
+
+  const loadItems = (day) => {
+    setTimeout(() => {
+      for (let i = -15; i < 85; i++) {
+        const time = day.timestamp + i * 24 * 60 * 60 * 1000;
+        const strTime = timeToString(time);
+        if (!items[strTime]) {
+          items[strTime] = [];
+          const numItems = Math.floor(Math.random() * 3 + 1);
+          for (let j = 0; j < numItems; j++) {
+            items[strTime].push({
+              name: 'Item for ' + strTime + ' #' + j,
+              height: Math.max(50, Math.floor(Math.random() * 150))
+            });
+          }
+        }
+      }
+      const newItems = {};
+      Object.keys(items).forEach(key => {
+        newItems[key] = items[key];
+      });
+      
+     setItems(newItems);
+    }, 1000);
+  };
+
+  const renderItem = (item) => {
+
+    return (
+    <TouchableOpacity style={{marginRight: 10, marginTop: 17}}
+    onPress={() => Alert.alert(item.name)}> 
+      <Card>
+        <Card.Content>
+        <View
+        style ={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alingItems: 'center',
+        }}>
+
+          <Text>{item.name}</Text>
+          <Avatar.Text label="N"/>
+        </View>
+        </Card.Content>
+      </Card>
+    </TouchableOpacity>
+    );
+
+  };
+
+  return ( <View style={{flex: 1}}>
+     <Agenda
+        items={items}
+        loadItemsForMonth={loadItems}
+        selected={'2021-07-11'}
+        renderItem={renderItem}
+       />
+        
+       
+  </View>);
+
+};
+
+export default  App
+ 
 
 const styles = StyleSheet.create({
   container: {
